bea1f32c9d33018b2569c0e19ed7f0585d1e2615
Harvey <probe@to.nexus>
2025-03-28 21:40:26 +0900

    2.19.7 add trustworthy verifyUrl

--- packages/core/src/constants/verify.ts
+++ packages/core/src/constants/verify.ts
@@ -1,6 +1,7 @@
 export const VERIFY_CONTEXT = "verify-api";
 
-export const VERIFY_SERVER = `https://dev-cross-verify.crosstoken.io`;
+export const VERIFY_SERVER_DEV = `https://dev-cross-verify.crosstoken.io`;
+export const VERIFY_SERVER = `https://cross-verify.crosstoken.io`;
 export const VERIFY_SERVER_V3 = `${VERIFY_SERVER}`;
 
-export const TRUSTED_VERIFY_URLS = [VERIFY_SERVER];
+export const TRUSTED_VERIFY_URLS = [VERIFY_SERVER_DEV, VERIFY_SERVER];
