abe07ff0164eac1385e5d07585b92735e0e090d3
GitHub Action <action@github.com>
2025-10-28 01:59:10 +0000

    chore: set workspace version to 1.17.7-alpha.4

--- packages/appkit/exports/constants.ts
+++ packages/appkit/exports/constants.ts
@@ -1,1 +1,1 @@
-export const PACKAGE_VERSION = '1.17.7-alpha.3'
+export const PACKAGE_VERSION = '1.17.7-alpha.4'
--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = '1.17.7-alpha.3'
+export const SDK_VERSION = '1.17.7-alpha.4'
