f43636c311f5701281ffb3172b84bac4cb27a47d
GitHub Action <action@github.com>
2025-12-02 07:56:30 +0000

    chore: set workspace version to 1.18.4-beta.0

--- packages/sdk/exports/index.ts
+++ packages/sdk/exports/index.ts
@@ -1,1 +1,1 @@
-export const sdkVersion = '1.18.4-alpha.0';export * from '../src/index.js'
+export const sdkVersion = '1.18.4-beta.0';export * from '../src/index.js'
--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = '1.18.4-alpha.0'
+export const SDK_VERSION = '1.18.4-beta.0'
