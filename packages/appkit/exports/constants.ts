08d195c3b02586564a2477d0b6b0ce0704a2b52d
GitHub Action <action@github.com>
2025-12-22 11:12:48 +0000

    chore: set workspace version to 1.19.0-beta.2

--- packages/appkit/exports/constants.ts
+++ packages/appkit/exports/constants.ts
@@ -1,1 +1,1 @@
-export const PACKAGE_VERSION = '1.19.0-beta.1'
+export const PACKAGE_VERSION = '1.19.0-beta.2'
--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = '1.19.0-beta.1'
+export const SDK_VERSION = '1.19.0-beta.2'
